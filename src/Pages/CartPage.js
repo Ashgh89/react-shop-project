bf64a8490fed79aa12c2d3525af68433a27c8dee
Ash <ghaneiash@gmail.com>
2022-05-17 16:55:24 +0200

    changes

--- src/Pages/CartPage.js
+++ src/Pages/CartPage.js
@@ -1,9 +1,22 @@
 import Layout from "../Layout/Layout";
+import { useCart } from "../Providers/CartProvider";
 
 const CartPage = () => {
+  // const cartState = useCart();
+  const { cart } = useCart();
+  // console.log(cartState.cart);
+  if (!cart)
+    return (
+      <main>
+        <h2>cart is empty</h2>
+      </main>
+    );
+
   return (
     <Layout>
-      <main>This is Cart Page</main>
+      <main className="container">
+        <section></section>
+      </main>
     </Layout>
   );
 };
